04bd1dcfc2b1eaaf935045958d8b0e4e55b1af5b
yusef <yusef@yusefs-MacBook-Air.local>
2024-10-06 14:26:08 +0300

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: '/hyber-auto-demo',
+    output: 'export', // Add this line for static export
+
+};
+
+export default nextConfig;
+
